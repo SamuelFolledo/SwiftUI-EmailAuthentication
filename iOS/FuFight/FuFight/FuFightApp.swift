9a2193d52fdcf88edc5b799368ed2d3b833dc2ff
Samuel Folledo <samuelfolledo@gmail.com>
2024-02-15 21:45:21 -0700

    initialize xcode project

---
+++ iOS/FuFight/FuFight/FuFightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuFightApp.swift
+//  FuFight
+//
+//  Created by Samuel Folledo on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FuFightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
